1916f7a881aa556feba5a49653bb669dd9a9b938
GermanPlaksunov <snipe9029quant@gmail.com>
2022-08-16 21:45:53 +0300

    develop --> HeroGameEngine added.

---
+++ HeroGameEngine/src/main/java/org/game/herogameengine/HeroGameEngineApplication.java
@@ -0,0 +1,13 @@
+package org.game.herogameengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeroGameEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeroGameEngineApplication.class, args);
+    }
+
+}
